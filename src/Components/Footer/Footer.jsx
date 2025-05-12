952f008e5061e628b83559cbac912bd6c671586a
nafijur-rahaman <20234103116@cse.bubt.edu.bd>
2025-05-13 02:51:50 +0600

    make counter and footer components

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Footer = () => {
     return (
-        <div>
-            {/* this is footer */}
-        </div>
+<footer className="footer flex flex-col items-center bg-black text-white p-10 ">
+  <div className="footer-logo flex items-center gap-3">
+    <img src="./logo-footer.png" alt="Logo" className="w-10 h-10" />
+    <h1 className="text-2xl font-bold">Better Call Lawyer</h1>
+  </div>
+
+  <ul className="flex gap-5">
+    <Link to="/"><li><button className="cursor-pointer">Home</button></li></Link>
+    <Link to="/bookings"><li><button className="cursor-pointer">My-Bookings</button></li></Link>
+    <Link to="/blogs"><li><button className="cursor-pointer">Blogs</button></li></Link>
+    <Link to="/error"><li><button className="cursor-pointer">Contact Us</button></li></Link>
+  </ul>
+
+
+<div className="w-full h-px border-t border-dashed border-[#FFFFFF20] my-4"></div>
+
+  <aside>
+    <p>Copyright © {new Date().getFullYear()} - All rights reserved</p>
+  </aside>
+</footer>
+
     );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../../Components/Banner/Banner';
 import Lawyers from '../../Components/Lawyers/Lawyers';
 import { useLoaderData } from 'react-router';
+import Counter from '../../Components/Counter/Counter';
 
 const Home = () => {
     const data=useLoaderData();
@@ -10,6 +11,7 @@ const Home = () => {
         <div>
           <Banner></Banner>
           <Lawyers lawyers={data}></Lawyers>
+          <Counter></Counter>
         </div>
     );
 };
